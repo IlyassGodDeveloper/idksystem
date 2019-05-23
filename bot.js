061f5ee506a9c0acfd6afff2b674046eaeff27b8
Nassim555 <48863215+Nassim555@users.noreply.github.com>
2019-05-23 15:14:39 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,22 +51,20 @@ if (message.content.startsWith(prefix + 'setgame')) {
 });
 
 client.on('message', message => {
- var mention = message.mentions.members.first();
-   var chan = message.member.voiceChannel;
-	var chan2 = mention.voiceChannel;
+  
   if (message.content.startsWith(prefix + "move")) {
-   if(!message.member.roles.find(role => role.name === "IDK-Move")) return message.reply("__**``ĮDK-Move`` ليس لديك رول**__");
-   if (!mention) return message.reply("__**العضو غير محدد**__");
-   if (!chan2) return message.reply("__**العضو غير مجود في في قناة صوتية**__")
+   if(!message.member.roles.find(role => role.name === "IDK-Move")) return message.reply("**``ĮDK-Move`` ليس لديك رول**");
+   var mention = message.mentions.members.first();
+   var chan = message.member.voiceChannel;
     console.log('hi')
     let log = message.guild.channels.find( channel => channel.name === "move-log");
-    log.send('**`' + message.author.username + '`** ** Moved ** **`' + mention.displayName + '`** ** From ** __**' +  chan2 +'**__ **To** __**' + chan + '**__');
+    log.send("'``' + message.author.username + '``''**' Moved '**' '``' + mention.displayName + '``''**' to'**' '__**' +  chan +'**__'");
     mention.setVoiceChannel(chan)
    
+   
   }
-
+    
 });
-
 
 const queue = new Map();
 client.on('ready', function() {
