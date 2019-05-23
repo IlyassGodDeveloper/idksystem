d99fa5bc26dbddb128a01aace08b91537f318b31
Nassim555 <48863215+Nassim555@users.noreply.github.com>
2019-05-23 14:54:22 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,21 @@ if (message.content.startsWith(prefix + 'setgame')) {
 });
 
 
-
+client.on('message', message => {
+  
+  if (message.content.startsWith(prefix + "move")) {
+   if(!message.member.roles.find(role => role.name === "IDK-Move")) return message.reply("**``ĮDK-Move`` ليس لديك رول**");
+   var mention = message.mentions.members.first();
+   var chan = message.member.voiceChannel;
+    console.log('hi')
+    let log = message.guild.channels.find( channel => channel.name === "move-log");
+    log.send("'``' + message.author.username + '``''**' Moved '**' '``' + mention.displayName + '``''**' to'**' '__**' +  chan +'**__'");
+    mention.setVoiceChannel(chan)
+   
+   
+  }
+    
+});
 
 const queue = new Map();
 client.on('ready', function() {
