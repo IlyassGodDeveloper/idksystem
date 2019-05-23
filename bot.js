2c81a521358930572811a6082a7177d1a7265a18
Nassim555 <48863215+Nassim555@users.noreply.github.com>
2019-05-23 14:32:10 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-const prefix = '+'
+const prefix = '+';
 
 client.on('message', message => {
  var mention = message.mentions.members.first();
    var chan = message.member.voiceChannel;
 	var chan2 = mention.voiceChannel;
-  if (message.content.startsWith(prefix + "move")) {
+  if (message.content.startsWith("+move")) {
    if(!message.member.roles.find(role => role.name === "IDK-Move")) return message.reply("__**``ĮDK-Move`` ليس لديك رول**__");
    if (!mention) return message.reply("__**العضو غير محدد**__");
    if (!chan2) return message.reply("__**العضو غير مجود في في قناة صوتية**__")
