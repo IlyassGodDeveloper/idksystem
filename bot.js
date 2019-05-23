70b6c6def5d252ae7f06afa54e14c3730384cf20
Nassim555 <48863215+Nassim555@users.noreply.github.com>
2019-05-23 14:08:21 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,14 +3,21 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '+'
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', message => {
+ var mention = message.mentions.members.first();
+   var chan = message.member.voiceChannel;
+	var chan2 = mention.voiceChannel;
+  if (message.content.startsWith(prefix + "move")) {
+   if(!message.member.roles.find(role => role.name === "IDK-Move")) return message.reply("__**``ĮDK-Move`` ليس لديك رول**__");
+   if (!mention) return message.reply("__**العضو غير محدد**__");
+   if (!chan2) return message.reply("__**العضو غير مجود في في قناة صوتية**__")
+    console.log('hi')
+    let log = message.guild.channels.find( channel => channel.name === "move-log");
+    log.send('**`' + message.author.username + '`** ** Moved ** **`' + mention.displayName + '`** ** From ** __**' +  chan2 +'**__ **To** __**' + chan + '**__');
+    mention.setVoiceChannel(chan)
+   
   }
+
 });
 //////////////////////////////////////////////////////////////////////
 
